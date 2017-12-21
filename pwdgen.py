51125248ef5a775f6b72679de46a85cb07775ea9
CBSkarmory <CBSkarmory@users.noreply.github.com>
2017-12-21 18:47:19 -0500

    error handling

--- pwdgen.py
+++ pwdgen.py
@@ -1,18 +1,34 @@
-import random, os
+import random, os, sys
 
 def gen(length):
-    tmp = [None] * length
+    tmp= [None] * length
     for c in range(length):
-        tmp[c] = chars[random.randint(0,len(chars)-1)]
+        tmp[c]= chars[random.randint(0,len(chars)-1)]
     return ''.join(tmp)
 
 if __name__ == "__main__":
-    print("================================\n   Random Password Generator\n================================")
-    print("recommendation: length > 8")
-    length = int(input("length:"))
-    reps = int(input("reps:"))
-    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*"
-    sysout = open("out.txt",'w')#opens out.txt in write mode
+    argv= sys.argv
+    argc= len(argv)
+    try:
+        if argc == 1:
+            print("================================\n   Random Password Generator\n================================")
+            print("recommendation: length > 8")
+            length= int(input("length:"))
+            reps= int(input("reps:"))
+        elif argc == 3:
+        
+            length= int(argv[1])
+            reps= int(argv[2])
+        else:
+            print("[ERROR] Invalid number of arguments")
+            print("[NOTE]  Usage is as follows: (python) pwdgen.py [length] [reps]")
+            sys.exit(2)
+    except ValueError:
+        print("[ERROR] Invalid argument detected; expected 2 numbers")
+        print("[NOTE]  Usage is as follows: (python) pwdgen.py [length] [reps]")
+        sys.exit(1)
+    chars= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*"
+    sysout= open("out.txt",'w')#opens out.txt in write mode
     for i in range(reps):
         sysout.write(gen(length))
         sysout.write("\n")

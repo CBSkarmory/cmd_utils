4d64bdace25be296d715c16b61b9312d613525ed
CBSkarmory <cbskarmory@users.noreply.github.com>
2019-09-04 23:01:33 -0400

    wav to mp3 utility

---
+++ wav_to_mp3.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+from sys import argv
+import multiprocessing
+import subprocess
+
+
+PROCESS_LIMIT = multiprocessing.cpu_count()
+
+
+def get_outfile_path(file_path: str) -> str:
+    if not os.path.exists(file_path):
+        raise ValueError(f'file {file_path} not found')
+    fn, ext = os.path.splitext(file_path)
+    ext = ext[1:]
+    if 'wav' != ext:
+        raise ValueError(f'expected wav file, got {ext} file')
+    out_path = f'{fn}.mp3'
+    print(out_path)
+    if os.path.exists(out_path):
+        raise ValueError(f'{out_path} exists already')
+    return out_path
+
+
+def cvt_single_wav(io_tup):
+    infile, outfile = io_tup
+    subprocess.run([
+        '/usr/bin/ffmpeg', '-i', infile, '-vn', '-ar', '44100',
+        '-ac', '2',
+        '-b:a', '320k',
+        outfile
+    ])
+
+
+def main():
+    argc = len(argv)
+    if argc < 2:
+        raise ValueError('expected at least 1 input')
+    out_paths = (get_outfile_path(p) for p in argv[1:])
+    par_args = list(zip(argv[1:], out_paths))
+    p: multiprocessing.Pool = multiprocessing.Pool(min(PROCESS_LIMIT, argc - 1))
+    p.map(cvt_single_wav, par_args)
+
+
+if __name__ == '__main__':
+    main()
